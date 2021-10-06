f1fca0b91f301704a0eaf6933db76d6f2b15c324
jeffngugi <ngugimwathi254@gmail.com>
2021-10-06 11:48:47 +0300

    chore(setup):package installation and folder structure setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+    return (
+        <View>
+            <Text>My app goes here</Text>
+        </View>
+    )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
